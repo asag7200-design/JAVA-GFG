8edcd18766ea570b22bb1e1a4c0b1c298f978c71
Sagar Choudhary <asag7200@gmail.com>
2026-04-21 01:24:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Even Odd Game/even-odd-game.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String args[]) {
+        // Your code here
+        int n =0;
+        if (n%2==0){
+            System.out.println("Friend");
+        }
+        else {
+            System.out.println("You");
+        }
+    }
+}
